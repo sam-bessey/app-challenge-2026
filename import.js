c68ad6bcb8a8d7330d8024044128c958d41e3dad
sam-bessey <samueljbessey@gmail.com>
2026-02-18 13:21:09 -0500

    Add working importing

--- import.js
+++ import.js
@@ -29,20 +29,28 @@ function importDrives(event) {
         // Convert it to an array
         const rows = reader.result.split("\n");
         let importedDrives = rows.map(row => {
-            return row.split(",")
+            return row.split(",");
         });
         importedDrives.shift(); // Remove the header row
 
         // Format it so it's compatible with the rest of the app
         for (let i = 0; i < importedDrives.length; i++) {
-            formattedDrives.push([convertTime(importedDrives[i][1]), importedDrives[i][0], importedDrives[i][2] !== "0"])
+            formattedDrives.push([convertTime(importedDrives[i][1]), importedDrives[i][0], importedDrives[i][2] !== "0"]);
         }
 
         console.log("New drives", formattedDrives);
-    }
+
+        // Hide file upload menu and show add/replace menu
+        document.getElementById("upload").hidden = true;
+        document.getElementById("controls").hidden = false;
+    };
+
+    // Show error message if something goes wrong
     reader.onerror = () => {
         alert("There was an error reading your file. Please try again");
-    }
+    };
+
+    // Read the file!
     reader.readAsText(file);
 }
 
@@ -54,5 +62,24 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("fileSelectButton").addEventListener("click", () => {
         document.getElementById("fileSelect").click();
     });
+
     document.getElementById("fileSelect").addEventListener("change", importDrives);
+
+    document.getElementById("replaceButton").addEventListener("click", () => {
+        // Replace drive list with new drives
+        const toSave = JSON.stringify(formattedDrives); // Format so it's ready to be saved
+        localStorage.setItem("drives", toSave); // And save it!
+    });
+
+    document.getElementById("keepButton").addEventListener("click", () => {
+        const drives = JSON.parse(localStorage.getItem("drives")) || []; // Get list of all drives
+
+        // Add these drives to the end of the list
+        for (let i = 0; i < formattedDrives.length; i++) {
+            drives.push(formattedDrives[i]);
+        }
+
+        const toSave = JSON.stringify(drives); // Format so it's ready to be saved
+        localStorage.setItem("drives", toSave); // And save it!
+    });
 });
